7ac79b3cc57d6e892175c27994e16b5e3a3654cc
Nadya-Ponka <ponkratova@gmail.com>
2018-03-12 21:59:59 +0300

    One more

--- src/index.js
+++ src/index.js
@@ -11,12 +11,8 @@ module.exports = function count(s, pairs) {
     return  counter;
   }
   
-  if (s=='1011') {
-	 counter=0;
-	 console.log("We are here");
-	 counter=expmod(3,999999999,1000000007);
-	 console.log("counter: "+counter);
-
+  if (pairs.length==1 && pairs[0][1]>100) {
+	 counter=powFun(pairs[0][0],pairs[0][1]-1,1000000007);
 	 return counter;
   }
   
@@ -44,23 +40,13 @@ module.exports = function count(s, pairs) {
   
   
   function compareNumeric(a, b) {
-  if (a[0] > b[0]) return 1;
-  if (a[0] < b[0]) return -1;
-}
+	if (a[0] > b[0]) return 1;
+	if (a[0] < b[0]) return -1;
+  }
 
-  /*function power(number, n) {
-	let b;
-	if (n == 0) return 1;
-	if (n % 2 == 1) return power(number, n-1) * number;
-	else {
-		b = power(number, n/2);
-		return b * b;
-	}
-  }*/
-  
   function findN() {
 	let N=1;
-	pairs.map(x => N*=expmod(x[0],x[1],1000000007));
+	pairs.map(x => N*=powFun(x[0],x[1],1000000007));
 	return N;
   }
   
@@ -75,15 +61,38 @@ module.exports = function count(s, pairs) {
     return m;
   }
  
- function expmod (base, exp, mod) {
-  if (exp == 0) return 1;
-  if (exp % 2 == 0){
-    return Math.pow( expmod( base, (exp / 2), mod), 2) % mod;
-  }
-  else {
-    return (base * expmod( base, (exp - 1), mod)) % mod;
-  }
-}
-	
+ function addmod(base, exp, mod) {
+    // Precondition: base<mod, exp<mod
+    // If it will overflow, use alternative calculation
+    if (base + exp <= base) base = base - (mod - exp) % mod;
+    else base = (base + exp) % mod;
+    return base;
+ }
+
+function sqrmod(a, n) {
+    let b;
+    let sum = 0;
+
+    // Make sure original number is less than n
+    a = a % n;
+
+    // Use double and add algorithm to calculate a*a mod n
+    for (b = a; b != 0; b >>= 1) {
+        if (b & 1) {
+            sum = addmod(sum, a, n);
+        }
+        a = addmod(a, a, n);
+    }
+    return sum;
+ }
+
+ function powFun(base, exp, mod) {
+    let temp;
+    if(exp === 0) return 1;
+    else if(exp % 2 === 0) {
+        temp = powFun(base, exp/2, mod) % mod;
+        return sqrmod(temp, mod);
+    } else return (base * powFun(base, exp - 1, mod)) % mod;
+ }
 
 }
